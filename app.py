e3c4eef3436c7f080c342183c56b3922807a6762
chandhini2 <chandhinishri98@gmail.com>
2026-02-26 15:35:11 +0530

    checking revert

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 print("Hello world !")
-print("xyz")
 print("chanz")
 print("xyzzcc")
 print("chandhinishri")
 
 print("this is second day of git")
 print("making changes in UI in main branch")
-
 print("adding shri in shri branch")
 print("adding new line")
+print("checking revert 1 ")
